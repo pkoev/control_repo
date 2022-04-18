bde7f6ab8b6ba684b54984a68d8b91d2926bb0ea
Peter Koev <36954317+pkoev@users.noreply.github.com>
2022-04-18 17:23:49 +0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,20 @@ class profile::base {
   user {'admin':
     ensure => present,
   }
+  host {'master.puppet.vm':
+    ensure => present,
+    ip => '192.168.56.101',
+  }
+  host {'ghost.puppet.vm':
+    ensure => present,
+    ip => '192.168.56.102',
+  }
+  host {'warlord.puppet.vm':
+    ensure => present,
+    ip => '192.168.56.104',
+  }
+  host {'nemesis.puppet.vm':
+    ensure => present,
+    ip => '192.168.56.103',
+  }
 }
