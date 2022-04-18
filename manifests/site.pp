61a8dee27f2c4a4558255a6d3fda3571858e4714
Peter Koev <36954317+pkoev@users.noreply.github.com>
2022-04-18 17:19:54 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,3 +11,12 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
 }
+node 'ghost.puppet.vm' {
+  include role::app_server
+}
+node 'nemesis.puppet.vm' {
+  include role::app_server
+}
+node 'warlord.puppet.vm' {
+  include role::db_server
+}
