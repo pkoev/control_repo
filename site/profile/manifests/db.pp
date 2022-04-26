a375465cdf45918646312fba3b707bdd03dc1278
Peter Koev <36954317+pkoev@users.noreply.github.com>
2022-04-26 13:03:48 +0300

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -2,7 +2,7 @@ class profile::db {
   class mongodb::mongos {
     port    => 27018,
     verbose => true, 
-    configdb => ['configsvr1.example.com:27018'],
+    configdb => ['ghost.puppet.vm:27018'],
     create_admin => true,
     admin_username => 'tadmin',
     admin_password => 'tpass',
