61e64daed16fd2c0ac5bb455e5a7e20499de300f
Peter Koev <36954317+pkoev@users.noreply.github.com>
2022-04-26 12:59:16 +0300

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -1,3 +1,11 @@
 class profile::db {
-
+  class mongodb::mongos {
+    port    => 27018,
+    verbose => true, 
+    configdb => ['configsvr1.example.com:27018'],
+    create_admin => true,
+    admin_username => 'tadmin',
+    admin_password => 'tpass',
+    ensure => present,
+  } 
 }
